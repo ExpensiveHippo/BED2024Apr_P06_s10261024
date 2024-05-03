10cc96910a09e6e474143b08f3e6c6df2b09019b
Ryan <s10261024@connect.np.edu.sg>
2024-05-03 14:53:30 +0800

    add validation

--- week3/books-api-mvc/app.js
+++ week3/books-api-mvc/app.js
@@ -1,6 +1,8 @@
+// Import modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const booksController = require("./controllers/booksController");
+const validateBook = require("./middlewares/validateBook")
 
 // Initialize app
 const app = express();
@@ -12,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Define routes
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
-app.post("/books", booksController.createBook);
-app.put("/books/:id", booksController.updateBook);
+app.post("/books", validateBook, booksController.createBook);
+app.put("/books/:id",validateBook,  booksController.updateBook);
 app.delete("/books/:id", booksController.deleteBook);
 
 // Define port
