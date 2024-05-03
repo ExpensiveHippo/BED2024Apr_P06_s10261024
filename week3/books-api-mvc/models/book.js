aadbd97acafe7bc532c324b844be29fdfc10cdbf
Ryan <s10261024@connect.np.edu.sg>
2024-05-03 14:05:19 +0800

    remove whitespaces

--- week3/books-api-mvc/models/book.js
+++ week3/books-api-mvc/models/book.js
@@ -2,7 +2,7 @@
 const books = [
     { id: 1, title: "The Lord of the Rings", author: "J.R.R. Tolkien" },
     { id: 2, title: "Pride and Prejudice", author: "Jane Austen" },
-  ];
+];
   
   class Book {
     constructor(id, title, author) {
