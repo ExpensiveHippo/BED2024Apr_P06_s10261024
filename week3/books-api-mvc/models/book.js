e947ab8e9423a99d7e1446c01dc4473704104a65
Ryan <s10261024@connect.np.edu.sg>
2024-05-03 14:05:30 +0800

    remove whitespaces

--- week3/books-api-mvc/models/book.js
+++ week3/books-api-mvc/models/book.js
@@ -4,70 +4,70 @@ const books = [
     { id: 2, title: "Pride and Prejudice", author: "Jane Austen" },
 ];
   
-  class Book {
+class Book {
     constructor(id, title, author) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
+        this.id = id;
+        this.title = title;
+        this.author = author;
     }
 
     // Static methods allows us to call the utility functions of "Book" without instantiating a "Book" object
     
     static async getAllBooks() {
-      return books; 
+        return books; 
     }
     
     static async getBookById(id) {
-      const books = await this.getAllBooks(); 
-      const book = books.find((book) => book.id === id);
-      return book;
+        const books = await this.getAllBooks(); 
+        const book = books.find((book) => book.id === id);
+        return book;
     }
   
     static async createBook(newBookData) {
-      const books = await this.getAllBooks(); 
-      const newBook = new Book(
-        books.length + 1,
-        newBookData.title,
-        newBookData.author
-      );
+        const books = await this.getAllBooks(); 
+        const newBook = new Book(
+            books.length + 1,
+            newBookData.title,
+            newBookData.author
+        );
      
-      books.push(newBook); 
-      return newBook;
+        books.push(newBook); 
+        return newBook;
     }
   
     static async updateBook(id, newBookData) {
-      const books = await this.getAllBooks(); 
-      const existingBookIndex = books.findIndex((book) => book.id === id);
-      if (existingBookIndex === -1) {
-        return null; // Book not found
-      }
-      
-      // "..." -> spread operator -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#overriding_properties
-      // 1. Spread the fields of the old book
-      // 2. Spread the fields of the new book
-      // 3. Merge the old book data with the new book data   
+        const books = await this.getAllBooks(); 
+        const existingBookIndex = books.findIndex((book) => book.id === id);
+        if (existingBookIndex === -1) {
+            return null; // Book not found
+        }
+        
+        // "..." -> spread operator -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#overriding_properties
+        // 1. Spread the fields of the old book
+        // 2. Spread the fields of the new book
+        // 3. Merge the old book data with the new book data   
 
-      const updatedBook = {
-        ...books[existingBookIndex],
-        ...newBookData,
-      };
+        const updatedBook = {
+            ...books[existingBookIndex],
+            ...newBookData,
+        };
 
-      console.log(updatedBook)
-  
-      books[existingBookIndex] = updatedBook;
-      return updatedBook;
+        console.log(updatedBook)
+    
+        books[existingBookIndex] = updatedBook;
+        return updatedBook;
     }
   
     static async deleteBook(id) {
-      const books = await this.getAllBooks(); 
-      const bookIndex = books.findIndex((book) => book.id === id);
-      if (bookIndex === -1) {
-        return false; // Book not found
-      }
-  
-      books.splice(bookIndex, 1);
-      return true;
+        const books = await this.getAllBooks(); 
+        const bookIndex = books.findIndex((book) => book.id === id);
+        if (bookIndex === -1) {
+            return false; // Book not found
+        }
+    
+        books.splice(bookIndex, 1);
+        return true;
     }
-  }
+}
   
-  module.exports = Book;
+module.exports = Book;
