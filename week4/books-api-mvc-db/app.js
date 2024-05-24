947997d73e0ab6aed7775d9946c728bd75fc8652
Ryan <s10261024@connect.np.edu.sg>
2024-05-24 15:36:36 +0800

    complete week 6 practical

--- week4/books-api-mvc-db/app.js
+++ week4/books-api-mvc-db/app.js
@@ -5,6 +5,7 @@ const bp = require("body-parser");
 
 // Import controller
 const booksController = require("./controllers/booksController");
+const usersController = require("./controllers/usersController");
 
 // Import database login configuration
 const dbConfig = require("./dbConfig");
@@ -24,12 +25,21 @@ app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(staticMiddleware);
 
-// Routes
+// Routes - Books
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
 app.post("/books", validateBook, booksController.createBook); 
 app.put("/books/:id", validateBook, booksController.updateBook);
 app.delete("/books/:id", booksController.deleteBook);
+
+// Routes - Users
+app.post("/users", usersController.createUser); 
+app.get("/users", usersController.getAllUsers); 
+app.get("/users/search", usersController.searchUsers); 
+app.get("/users/with-books", usersController.getUsersWithBooks);
+app.get("/users/:id", usersController.getUserById); 
+app.put("/users/:id", usersController.updateUser);  
+app.delete("/users/:id", usersController.deleteUser);
 
 app.listen(PORT, async() => {
     try {
---
+++ week4/books-api-mvc-db/controllers/usersController.js
@@ -0,0 +1,100 @@
+const User = require("../models/user");
+
+const createUser = async (req, res) => {
+    const newUser = req.body;
+
+    try {
+        const createdUser = await User.createUser(newUser);
+        res.status(201).json(createdUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error creating user");
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.getAllUsers();
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error retrieving users");
+    }
+}
+
+const getUserById = async (req, res) => {
+    const userId = parseInt(req.params.id); 
+    try {
+        const user = await User.getUserById(userId);
+        if (!user) {
+            res.status(404).send("User not found");
+        }
+        res.json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error retrieving user");
+    }
+}
+
+const updateUser = async (req, res) => {
+    const userId =  parseInt(req.params.id);
+    const newUser = req.body;
+
+    try {
+        const updatedUser = await User.updateUser(userId, newUser);
+        if (!updatedUser) {
+            return res.status(404).send("User not found");
+        }
+        res.json(updatedUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating user");
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const userId = parseInt(req.params.id);
+
+    try {
+        const success = await User.deleteUser(userId);
+        if (!success) {
+            return res.status(404).send("User not found");
+        }
+        res.status(204).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error deleting user");
+    }
+}
+
+const searchUsers = async (req, res) => {
+    const searchTerm = req.query.searchTerm; 
+  
+    try {    
+        const users = await User.searchUsers(searchTerm);
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error searching users" });
+    }
+}
+
+async function getUsersWithBooks(req, res) {
+    try {
+        const users = await User.getUsersWithBooks();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching users with books" });
+    }
+}
+
+module.exports = {
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+    searchUsers,
+    getUsersWithBooks
+}
